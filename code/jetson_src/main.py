49572e1c2c73dc766c4c2b0b6aa147466dccde80
Eduard <atodireseieduardiulian@gmail.com>
2024-07-21 16:36:22 +0300

    Created initial directory structure

---
+++ code/jetson_src/main.py
@@ -0,0 +1,9 @@
+"""
+This is the main file that handles the script for image recognition, commands transmitted to arduino, etc.
+"""
+
+import sys
+import cv2
+
+print(sys.version)
+print("adasdasd")
